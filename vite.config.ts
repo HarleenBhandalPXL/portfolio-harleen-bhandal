458a99ae558fd1d8438ed3700023e245e8f732fe
HarleenBhandalPXL <harleen.bhandal@student.pxl.be>
2025-04-24 22:10:51 +0200

    Changed Title

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-harleen-bhandal/',
   plugins: [
     vue(),
     vueDevTools(),
